69eba328085d4ada24a6983bd993e32e638ed6a6
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-07-06 19:19:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,25 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
 const Text = styled.h1`
   color: white;
+`;
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 function App() {
   return (
     <Father>
-      <BoxOne>
+      <Box bgColor={"tomato"}>
         <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      </Box>
+      <Circle bgColor={"teal"} />
     </Father>
   );
 }
