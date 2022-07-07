d57bf083e6d733f5da9f250fa48f7c7f729752f9
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-07-07 16:23:45 +0900

    #2,7 themes

--- src/App.js
+++ src/App.js
@@ -2,44 +2,23 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
- from{
- transform:rotate(0deg);
- border-radius:0px;
- }to{
- transform:rotate(360deg);
- border-radius:100px;
- }
-`;
-const Hello = styled.span`
-  font-size: 30px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 0.5s linear infinite;
-  text-align: center;
-  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
   justify-content: center;
   align-items: center;
-  ${Hello} {
-    color: blue;
-  }
-  &:hover ${Hello} {
-    background-color: red;
-  }
 `;
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Hello>hello</Hello>
-      </Box>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Title>title</Title>
+      </Wrapper>
+    </>
   );
 }
 
