6483b6f02876a1b19a7fc2751f688088b2d17ae8
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-07-19 11:05:36 +0900

    Color update

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container =styled.div`
+  
+`
+const Header=styled.header``
+const CoinList = styled.ul``
+const Coin = styled.li``
 const Title = styled.h1`
   color:${props=>props.theme.accentColor};
 `
 const Coins = () => {
   return (
-    <Title>Coins</Title>
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinList>
+        <Coin></Coin>
+      </CoinList>
+    </Container>
   )
 }
 
