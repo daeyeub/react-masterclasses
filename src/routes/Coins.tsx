56d6b2b0bb2c7befdb0c966bee4703b41a175ff0
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-08-13 11:29:40 +0900

    2022-08-13

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -22,11 +22,12 @@ const Header = styled.header`
 `;
 const CoinsList = styled.ul``;
 const Coin = styled.li`
-  background-color: white;
+  background-color: ${(props) => props.theme.cardBgColor};
   color: ${(props) => props.theme.textColor};
   margin-bottom: 10px;
   padding: 10px;
   border-radius: 15px;
+  border: 1px solid white;
   a {
     transition: color 0.2s ease-in-out;
     padding: 20px;
